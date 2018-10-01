fa5bce3ebc40b54dbcf740cf158cea881b3ac9ee
mareksl <marekslad@hotmail.com>
2018-10-01 14:54:56 +0200

    styling

--- src/app/pages/dashboard/panel/panel.component.ts
+++ src/app/pages/dashboard/panel/panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panel.component.scss']
 })
 export class PanelComponent implements OnInit {
+  @Input()
+  type: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
