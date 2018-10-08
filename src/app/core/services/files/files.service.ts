f59920be99af0288edce525bf789687d5e265e8b
mareksl <marekslad@hotmail.com>
2018-10-08 21:26:14 +0200

    add xlsx download functionality

--- src/app/core/services/files/files.service.ts
+++ src/app/core/services/files/files.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from 'src/app/config/app-settings.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  download() {
+    return this.http.get(`${config.apiUrl}/files`, {
+      responseType: 'blob',
+      observe: 'response'
+    });
+  }
 }
--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FilesService } from 'src/app/core/services/files/files.service';
+import { Subscription } from 'rxjs';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-admin',
@@ -8,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
 export class AdminComponent implements OnInit {
   file: File;
 
-  constructor() {}
+  downloadSub: Subscription;
+
+  constructor(private filesService: FilesService) {}
 
   ngOnInit() {
     this.file = null;
+  }
+
+  downloadFile() {
+    this.filesService.download().subscribe(res => {
+      const filename = res.headers
+        .get('Content-Disposition')
+        .split(';')[1]
+        .split('=')[1]
+        .replace(/['"]/g, '');
+      const blob = new Blob([res.body], { type: res.body.type });
+      saveAs(blob, filename);
+    });
   }
 
   uploadFile() {
