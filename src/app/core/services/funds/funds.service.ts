7f4b64320bb4b92306c681cea980903e98f90141
mareksl <marekslad@hotmail.com>
2018-10-12 14:14:51 +0200

    fund-details component

--- src/app/core/services/funds/funds.service.ts
+++ src/app/core/services/funds/funds.service.ts
@@ -4,9 +4,13 @@ import { Fund, IFund } from '../../../shared/models/fund.model';
 import { config } from '../../../config/app-settings.config';
 import { map, catchError } from 'rxjs/operators';
 import { handleError } from '../../../shared/handlers/error.handler';
+import { Observable } from 'rxjs';
 
 export interface FundsResponse {
   funds: IFund[];
+}
+export interface FundResponse {
+  fund: IFund;
 }
 
 @Injectable({
@@ -15,10 +19,16 @@ export interface FundsResponse {
 export class FundsService {
   constructor(private http: HttpClient) {}
 
-  getAll() {
+  getAll(): Observable<Fund[]> {
     return this.http.get<FundsResponse>(`${config.apiUrl}/funds`).pipe(
       map(fundsResponse => fundsResponse.funds.map(fund => new Fund(fund))),
       catchError(handleError('getAll', []))
+    );
+  }
+  getById(id: number): Observable<Fund> {
+    return this.http.get<FundResponse>(`${config.apiUrl}/funds/${id}`).pipe(
+      map(fundResponse => new Fund(fundResponse.fund)),
+      catchError(handleError('getById', null))
     );
   }
 
--- src/app/pages/funds/fund-details/fund-details.component.ts
+++ src/app/pages/funds/fund-details/fund-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Fund } from 'src/app/shared/models/fund.model';
+import { ActivatedRoute, Params } from '@angular/router';
+import { FundsService } from 'src/app/core/services/funds/funds.service';
 
 @Component({
   selector: 'app-fund-details',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fund-details.component.scss']
 })
 export class FundDetailsComponent implements OnInit {
+  fund: Fund;
+  id: number;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private fundsService: FundsService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.getFund(this.id);
+    });
   }
 
+  getFund(id: number) {
+    this.fundsService.getById(id).subscribe(fund => (this.fund = fund));
+  }
 }
